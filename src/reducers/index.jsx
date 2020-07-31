6307a9e50a24f78bd8ec566fa27f6c7809dc037a
raju-ahammad <web.rajuahammad@gmail.com>
2020-08-01 01:32:13 +0600

    update postList

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux'
+import PostReducers from './PostReducers'
 
-export default combineReducers({})
+export default combineReducers({
+    posts: PostReducers
+})
