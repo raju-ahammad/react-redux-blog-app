bc340d68cc9d0f7ea91b9500683c811dd3bab7a4
raju-ahammad <web.rajuahammad@gmail.com>
2020-08-03 02:26:35 +0600

    add lodash

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,11 +1,15 @@
 import jsonPlaceholder from '../Api/jsonPlaceholder'
+import _ from  'lodash'
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get("/posts")
     dispatch({ type: "FETCH_POST", payload: response.data })
 }
 
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+ const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
+
     dispatch({ type: "FETCH_USER", payload: response.data })
-}
+ });
